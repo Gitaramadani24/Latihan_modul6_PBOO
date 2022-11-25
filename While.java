7be86adb4b0143cd31a0098de7c34a43251a440e
Gitaramadani24 <114290893+Gitaramadani24@users.noreply.github.com>
2022-11-25 10:06:57 +0700

    Add files via upload

---
+++ While.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_modul6_pbo;
+
+/**
+ *
+ * @author user
+ */
+public class While {
+ public static void main(String args[]){
+  int r=0;
+  while (r<100){
+   System.out.println("Gita Ramadani");
+   r++;
+  }
+ }
+}
